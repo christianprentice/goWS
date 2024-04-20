25dee8e821a9d5068686603cd568561b18cee57b
Christian <purin213@hotmail.com>
2024-04-20 15:05:31 +0900

    basic websocket

--- main.go
+++ main.go
@@ -1,23 +1,50 @@
 package main
 
 import(
-    "golang.org/x/net/websocket"
     "fmt"
+    "io"
+    "net/http"
+    "golang.org/x/net/websocket"
 )
 
 type Server struct {
-    connections map[*websocket.Conn]bool
+    conn map[*websocket.Conn]bool
 }
 
 func NewServer() *Server {
     return &Server{
-        connections: make(map[*websocket.Conn]bool),
+        conn: make(map[*websocket.Conn]bool),
     }
 }
 
 func (s *Server) handleWS(ws *websocket.Conn) {
-    fmt.println("new connection from client:", ws.RemoteAddr())
+    fmt.Println("new connection from client:", ws.RemoteAddr())
+
+    s.conn[ws] = true
+
+    s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+    buf := make([]byte, 1024)
+    for {
+        n, err := ws.Read(buf)
+        if err != nil {
+            if err == io.EOF {
+                break
+            }
+            fmt.Println("read error:", err)
+            continue
+        }
+        msg := buf[:n]
+        fmt.Println(string(msg))
+        ws.Write([]byte("message sent"))
+    }
 }
 
 func main() {
+    server := NewServer()
+    http.Handle("/ws", websocket.Handler(server.handleWS))
+    http.ListenAndServe(":4444", nil)
 }
+
